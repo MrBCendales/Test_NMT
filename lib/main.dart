9882c1e65023bf1ee657ffd7735ede1de5886e69
MrBCendales <bn.cendales10@gmail.com>
2024-11-12 18:12:04 +0900

    feat: Change title

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'OMG THIS IS WORKINGGG!!!!'),
+      home: const MyHomePage(title: 'Hello Tugu-Sir, give me coffee sir!'),
     );
   }
 }
