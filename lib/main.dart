3702971a9ffc1c7dde0aee6d294e767d50821641
MrBCendales <bn.cendales10@gmail.com>
2024-11-12 18:06:52 +0900

    feat: Change title

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Based in AWS, Testing Github Application'),
+      home: const MyHomePage(title: 'OMG THIS IS WORKINGGG!!!!'),
     );
   }
 }
